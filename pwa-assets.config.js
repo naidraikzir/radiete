78c43c246a0a7ac37a3098f2425d2bb3956d62f2
Rizki Ardian <radeite8@gmail.com>
2026-02-03 13:34:35 +0700

    update icon

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,6 +1,20 @@
 import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config';
 
 export default defineConfig({
-	preset,
+	preset: {
+		...preset,
+		apple: {
+			...preset.apple,
+			resizeOptions: {
+				background: 'transparent'
+			}
+		},
+		maskable: {
+			...preset.maskable,
+			resizeOptions: {
+				background: 'transparent'
+			}
+		}
+	},
 	images: ['static/favicon.svg']
 });
